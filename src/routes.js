e912738b4c808069c46987fa6e0441eacb6bab6c
Madhav Rathi <madrathi@amazon.com>
2021-01-30 23:03:58 +0530

    feature added

--- src/routes.js
+++ src/routes.js
@@ -2,55 +2,56 @@ const express = require('express');
 var admin = require('firebase-admin');
 const router = express.Router();
 
-var serviceAccount = require('../wocleaderboard-firebase.json');
+var serviceAccount = require("../wocleaderboard-firebase.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://wocleaderboard-default-rtdb.firebaseio.com',
+  databaseURL: "https://wocleaderboard-default-rtdb.firebaseio.com"
 });
 
 router.use((req, res, next) => {
   next();
 });
 
+
 router.post('/updateLeaderBoard', (request, response) => {
-  console.log('updating leaderboard logs!', request);
-  const ref = admin.database().ref('/' + request.body.user);
-  const score =
-    request.body.score === '' ? 0 : -1 * parseInt(request.body.score);
-  ref.once('value').then((snapshot) => {
+  console.log("updating leaderboard logs!", request);
+  if(!request.body.user || !request.body.score ) {
+    response.send("Haha nice try. Better spend time buying GME");
+  }
+  const ref = admin.database().ref("/" + request.body.user);
+  ref.once("value").then((snapshot) => {
     if (snapshot.val() == null) {
       ref.set({
-        score: score,
+        score: -1*(parseInt(request.body.score)),
       });
     } else {
-      const newScore =
-        parseInt(snapshot.val().score) + -1 * parseInt(request.body.score);
+    	const newScore = parseInt(snapshot.val().score) + -1*parseInt(request.body.score);
       ref.set({
-        score: newScore,
+	        score: newScore,
       });
     }
   });
 
-  response.send('Update Successfull');
+  response.send("Update Successfull");
 });
 
 router.get('/getLeaderBoard', (request, response) => {
-  console.log('fetching leaderboard logs!');
+  console.log("fetching leaderboard logs!");
 
-  const db = admin.database().ref('/');
+  const db = admin.database().ref("/");
   let fullDataDump;
 
-  db.orderByChild('score').once('value', function (snapshot) {
-    fullDataDump = snapshotToArray(snapshot);
-    response.status(200).json(fullDataDump);
+  db.orderByChild("score").once("value", function(snapshot) {
+	    fullDataDump = snapshotToArray(snapshot);
+	    response.status(200).json(fullDataDump);
   });
 });
 
 function snapshotToArray(snapshot) {
   const returnArr = [];
 
-  snapshot.forEach(function (childSnapshot) {
+  snapshot.forEach(function(childSnapshot) {
     const item = childSnapshot.val();
     item.key = childSnapshot.key;
 
