6b05013db94052b93005906487d46b50f18de521
Madhav Rathi <madrathi@amazon.com>
2021-01-30 18:54:46 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors({origin: '*'}));
 
 const port = process.env.PORT || 8080;
 
