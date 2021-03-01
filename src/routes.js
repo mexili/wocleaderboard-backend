cfa47c5281cf5c2b92c48e39d3fa060f83f91cbb
Madhav Rathi <madrathi@amazon.com>
2021-03-01 19:31:12 +0530

    WOC season 1 - complete :)

--- src/routes.js
+++ src/routes.js
@@ -16,27 +16,27 @@ router.use((req, res, next) => {
 
 router.post('/updateLeaderBoard', (request, response) => {
   console.log("updating leaderboard logs!", request.body);
-  let data = request.body.score;
-  let user = data.split(":")[0].split("=")[1];
-  let score = data.split(":")[1].split("=")[1];
-  if(!user || !score ) {
-    response.send("Haha nice try. Better spend time buying GME");
-  }
-  const ref = admin.database().ref("/" + user);
-  ref.once("value").then((snapshot) => {
-    if (snapshot.val() == null) {
-      ref.set({
-        score: -1*(parseInt(score)),
-      });
-    } else {
-    	const newScore = parseInt(snapshot.val().score) + -1*parseInt(score);
-      ref.set({
-	        score: newScore,
-      });
-    }
-  });
+  // let data = request.body.score;
+  // let user = data.split(":")[0].split("=")[1];
+  // let score = data.split(":")[1].split("=")[1];
+  // if(!user || !score ) {
+  //   response.send("Haha nice try. Better spend time buying GME");
+  // }
+  // const ref = admin.database().ref("/" + user);
+  // ref.once("value").then((snapshot) => {
+  //   if (snapshot.val() == null) {
+  //     ref.set({
+  //       score: -1*(parseInt(score)),
+  //     });
+  //   } else {
+  //   	const newScore = parseInt(snapshot.val().score) + -1*parseInt(score);
+  //     ref.set({
+	//         score: newScore,
+  //     });
+  //   }
+  // });
 
-  response.send("Update Successfull");
+  response.send("Leaderboard Frozen, Looking forward to your contributions in next season :)");
 });
 
 router.get('/getLeaderBoard', (request, response) => {
